1321076557310667b3d06e064fddc8c51c24cc96
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-19 18:40:09 -0500

    Refactor: Removed methods for returned object of formInputs in js/form.js

--- js/form.js
+++ js/form.js
@@ -1,15 +1,1 @@
-import { sum, avg } from './arith.js';
-
-export const formInputs = ($form) => {
-  const inputs = Object.fromEntries(new FormData($form));
-
-  inputs.prototype.sum = function () {
-    return sum(Object.values(this).map((n = Number(n))));
-  };
-
-  inputs.prototype.avg = function () {
-    return avg(Object.values(this).map((n = Number(n))));
-  };
-
-  return inputs;
-};
+export const formInputs = (form) => Object.fromEntries(new FormData(form));
