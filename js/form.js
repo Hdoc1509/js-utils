9f334939f83fbe1ea84bf69e19d27592a80825ab
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-18 19:15:19 -0500

    Feat: New methods for returned object of formInputs in js/form.js

--- js/form.js
+++ js/form.js
@@ -1,1 +1,15 @@
-export const formInputs = (form) => Object.fromEntries(new FormData(form));
+import { sum, avg } from './arith.js';
+
+export const formInputs = ($form) => {
+  const inputs = Object.fromEntries(new FormData($form));
+
+  inputs.prototype.sum = function () {
+    return sum(Object.values(this).map((n = Number(n))));
+  };
+
+  inputs.prototype.avg = function () {
+    return avg(Object.values(this).map((n = Number(n))));
+  };
+
+  return inputs;
+};
