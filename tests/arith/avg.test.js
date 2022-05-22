56c156910c7101eec70f13f3cd1f41c9063381b5
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-22 13:06:28 -0500

    Test: Added test for avg() in utils/arith.js

---
+++ tests/arith/avg.test.js
@@ -0,0 +1,29 @@
+import { avg } from '../../utils/arith.js';
+
+describe('--- AVG ---', () => {
+  test('No arguments returns 0', () => {
+    expect(avg()).toBe(0);
+  });
+
+  test('Argment is not an array of numbers returns undefined', () => {
+    expect(avg('string is not a number')).toBeUndefined();
+
+    expect(avg(['string', {}, [null]])).toBeUndefined();
+  });
+
+  test('Empty array as argument returns 0', () => {
+    expect(avg([])).toBe(0);
+  });
+
+  test('Of 4 and 6', () => {
+    const numbers = [4, 6];
+
+    expect(avg(numbers)).toBe(5);
+  });
+
+  test('Of many numbers', () => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    expect(avg(numbers)).toBe(5.5);
+  });
+});
