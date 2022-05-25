3151a54babd3ab91daa578309a6885e813a3c5bd
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-24 19:01:47 -0500

    Simplified utils/arith.js

--- utils/arith.js
+++ utils/arith.js
@@ -3,15 +3,11 @@ import { arrayOfNumbers } from './array.js';
 export const sum = (nums = []) => {
   if (!arrayOfNumbers(nums)) return;
 
-  if (nums.length === 0) return 0;
-
   return nums.reduce((prev, current) => prev + current);
 };
 
 export const avg = (nums = []) => {
   if (!arrayOfNumbers(nums)) return;
-
-  if (nums.length === 0) return 0;
 
   return sum(nums) / nums.length;
 };
