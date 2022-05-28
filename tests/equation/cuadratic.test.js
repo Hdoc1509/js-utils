984b99f20e7dd5ba84c233b1103868c248763c0d
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-27 19:24:01 -0500

    Test: Added and passed test in cuadratic.test.js

--- tests/equation/cuadratic.test.js
+++ tests/equation/cuadratic.test.js
@@ -1,5 +1,15 @@
 import EQN from '../../utils/equation.js';
 
 describe('--- Cuadratic Equation ---', () => {
-  test.todo('Test for cuadratic eqn', () => {});
+  test('No argument returns {root1: 0, root2: 0}', () => {
+    expect(EQN.cuadratic()).toEqual({ root1: 0, root2: 0 });
+  });
+
+  test.todo('Parameter is not an object of coefficients returns undefined');
+
+  test('{coeA: 2, coeB: 1, coeC: 0} as parameter returns {root1: 0, root2: -0.5}', () => {
+    const coefficients = { coeA: 2, coeB: 1, coeC: 0 };
+
+    expect(EQN.cuadratic(coefficients)).toEqual({ root1: 0, root2: -0.5 });
+  });
 });
