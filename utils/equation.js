a2cb7b645c4aff259704b8f784512f74dd888a1f
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-27 19:22:33 -0500

    Simplified cuadratic() in utils/equation.js

--- utils/equation.js
+++ utils/equation.js
@@ -1,10 +1,13 @@
-const cuadratic = ({ coeA = 0, coeB = 0, coeC = 0 }) => {
+const cuadratic = ({ coeA = 1, coeB = 0, coeC = 0 } = {}) => {
   const discriminant = coeB ** 2 - 4 * coeA * coeC;
 
   if (discriminant < 0) return;
 
-  const root1 = (-coeB + Math.sqrt(discriminant)) / (2 * coeA);
-  const root2 = (-coeB - Math.sqrt(discriminant)) / (2 * coeA);
+  const root = (-coeB + Math.sqrt(discriminant)) / (2 * coeA);
+
+  const root1 = root;
+  const root2 =
+    discriminant === 0 ? root : (-coeB - Math.sqrt(discriminant)) / (2 * coeA);
 
   return { root1, root2 };
 };
