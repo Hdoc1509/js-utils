7487c248dbdfc0fde823e4483cfda1ce6eb7ae32
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-29 13:09:54 -0500

    Added a default export in dom.js

--- src/dom.js
+++ src/dom.js
@@ -7,3 +7,5 @@ export const qs = (selector, parent = d) => parent.querySelector(selector);
 export const qsa = (selector, parent = d) => [
   ...parent.querySelectorAll(selector),
 ];
+
+export default { gid, qs, qsa };
