7810cbd987a3c5d51d096de219716eb5286a6b27
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-29 13:13:14 -0500

    Added index.js file

---
+++ index.js
@@ -0,0 +1,5 @@
+export { sum, avg } from './dist/arith.js';
+export { arrayOfNumbers } from './dist/array.js';
+export { default as DOM } from './dist/dom.js';
+export { default as EQN } from './dist/equation.js';
+export { formInputs } from './dist/form.js';
