910cc68ee51de96580b1536b1ad8e4efa8304dc0
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-29 13:24:25 -0500

    Added single and default exports for arith.js and dom.js in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-export { sum, avg } from './dist/arith.js';
+export { default as ARITH, sum, avg } from './dist/arith.js';
 export { arrayOfNumbers } from './dist/array.js';
-export { default as DOM } from './dist/dom.js';
+export { default as DOM, gid, qs, qsa } from './dist/dom.js';
 export { default as EQN } from './dist/equation.js';
 export { formInputs } from './dist/form.js';
