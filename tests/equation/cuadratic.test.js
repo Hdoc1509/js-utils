996efe2c6b5e403c399aae22b426c31d4e5e9ac6
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-31 18:15:46 -0500

    Test: Modified test for undefined argument and added a new test in cuadratic.test.js

--- tests/equation/cuadratic.test.js
+++ tests/equation/cuadratic.test.js
@@ -1,11 +1,19 @@
 import EQN from '../../src/equation.js';
 
 describe('--- Cuadratic Equation ---', () => {
-  test('No argument returns {root1: 0, root2: 0}', () => {
-    expect(EQN.cuadratic()).toEqual({ root1: 0, root2: 0 });
+  test('No argument returns undefined', () => {
+    expect(EQN.cuadratic()).toBeUndefined();
   });
 
-  test.todo('Parameter is not an object of coefficients returns undefined');
+  test('Parameter is not an object of coefficients returns undefined', () => {
+    const coefficients = {
+      prop: 'not allowed',
+      example: null,
+      notAllowed: [1],
+    };
+
+    expect(EQN.cuadratic(coefficients)).toBeUndefined();
+  });
 
   test('{coeA: 2, coeB: 1, coeC: 0} as parameter returns {root1: 0, root2: -0.5}', () => {
     const coefficients = { coeA: 2, coeB: 1, coeC: 0 };
