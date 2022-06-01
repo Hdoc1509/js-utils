85fa4776ff8591896f6b5c55398ea720be0d7b47
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-01 18:36:43 -0500

    Test: Modified sort() of array.js and passed its tests in sort.test.js

--- src/array.js
+++ src/array.js
@@ -10,6 +10,18 @@ export const arrayOfNumbers = (arr = []) => {
   return true;
 };
 
+export const arrayOfStrings = (arr = []) => {
+  if (!Array.isArray(arr)) return false;
+
+  if (arr.length === 0) return false;
+
+  const notStrings = arr.filter((el) => typeof el !== 'string');
+
+  if (notStrings.length > 0) return false;
+
+  return true;
+};
+
 export const randomElement = (arr) => {
   if (!Array.isArray(arr)) return;
 
@@ -18,12 +30,29 @@ export const randomElement = (arr) => {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
+const validOrders = [1, -1, 'r'];
+
 export const sort = (arr = [], order = 1) => {
-  if (!Array.isArray(arr)) return;
+  if (!arrayOfNumbers(arr) && !arrayOfStrings(arr)) return;
 
-  if (arr.length < 2) return arr;
+  if (!validOrders.includes(order)) return;
 
-  if (order === 1) return arr.map((el) => el).sort((a, b) => a - b);
-  if (order === -1) return arr.map((el) => el).sort((a, b) => b - a);
-  if (order === 'r') return arr.map((el) => el).sort(() => Math.random());
+  if (arr.length === 1) return arr;
+
+  if (arrayOfNumbers(arr)) {
+    if (order === 1) return arr.map((el) => el).sort((a, b) => a - b);
+    if (order === -1) return arr.map((el) => el).sort((a, b) => b - a);
+    if (order === 'r') return arr.map((el) => el).sort(() => Math.random());
+  }
+
+  if (order === 1) return arr.map((el) => el).sort();
+  if (order === -1)
+    return arr
+      .map((el) => el)
+      .sort((a, b) => {
+        if (a > b) return -1;
+        if (b > a) return 1;
+        return 0;
+      });
+  if (order === 'r') return arr.map((el) => el).sort();
 };
