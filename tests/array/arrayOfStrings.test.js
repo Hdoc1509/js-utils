c25a5b37dd18a605b16e6b6617db95466a1a2477
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-01 18:44:25 -0500

    Test: Added and passed tests for arrayOfStrings() of array.js

---
+++ tests/array/arrayOfStrings.test.js
@@ -0,0 +1,23 @@
+import { arrayOfStrings } from '../../src/array.js';
+
+describe('--- Array of strings', () => {
+  test('No argument returns false', () => {
+    expect(arrayOfStrings()).toBe(false);
+  });
+
+  test('Argument is not an array of strings returns false', () => {
+    expect(arrayOfStrings(1)).toBe(false);
+
+    expect(arrayOfStrings([{}, [1, 2, 3], null])).toBe(false);
+  });
+
+  test('Empty array returns false', () => {
+    expect(arrayOfStrings([])).toBe(false);
+  });
+
+  test('Array of strings returns true', () => {
+    expect(
+      arrayOfStrings(['string', 'word', 'random', 'example of text'])
+    ).toBe(true);
+  });
+});
