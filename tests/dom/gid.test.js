712c2e61a849354a11ead2d67bd2a37923c16568
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-02 18:09:25 -0500

    Test: Added and passed test for gid() of dom.js

---
+++ tests/dom/gid.test.js
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { gid } from '../../src/dom.js';
+
+describe('--- DOM gid ---', () => {
+  document.body.innerHTML = `
+    <main>
+      <p id="text1">Text 1</p>
+      <p id="text2">Text 2</p>
+    </main>
+  `;
+
+  test('No argument returns null', () => {
+    const element = gid();
+
+    expect(element).toBeNull();
+  });
+
+  test('Getting the element by id correctly', () => {
+    const element1 = gid('text1');
+    const element2 = gid('text2');
+
+    expect(element1).toBeDefined();
+    expect(element1.textContent).toBe('Text 1');
+
+    expect(element2).toBeDefined();
+    expect(element2.textContent).toBe('Text 2');
+  });
+});
