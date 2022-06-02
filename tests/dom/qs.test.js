7f32268c90146bd56f7c5bfad653ec1b4e2ae47c
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-02 18:49:13 -0500

    Added and passed test for qs() of dom.js

---
+++ tests/dom/qs.test.js
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { qs } from '../../src/dom.js';
+
+describe('--- DOM qs ---', () => {
+  document.body.innerHTML = `
+    <main>
+      <p id="text1">Text 1</p>
+      <p id="text2">Text 2</p>
+    </main>
+  `;
+
+  test('No argument returns null', () => {
+    const $element = qs();
+
+    expect($element).toBeNull();
+  });
+
+  test('Getting the element by query selector correctly', () => {
+    const $main = qs('main');
+    const $paragraph1 = qs('p', $main);
+
+    expect($main).toBeDefined();
+    expect($main.children.length).toBe(2);
+
+    expect($paragraph1).toBeDefined();
+    expect($paragraph1.textContent).toBe('Text 1');
+  });
+});
