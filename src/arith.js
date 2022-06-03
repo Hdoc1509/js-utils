08bfa35970c9712bdc1a29f82427051d34c855ec
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-03 17:56:00 -0500

    Added jsdoc comments to sum() and avg() of arith.js

--- src/arith.js
+++ src/arith.js
@@ -1,11 +1,19 @@
 import { arrayOfNumbers } from './array.js';
 
+/**
+ * Returns the sum of an array of numbers
+ * @param  {Array<Number>}  nums Array of numbers to sum
+ */
 export const sum = (nums = []) => {
   if (!arrayOfNumbers(nums)) return;
 
   return nums.reduce((prev, current) => prev + current);
 };
 
+/**
+ * Returns the arithmetic average of an array of numbers
+ * @param  {Array<Number>}  nums Array of numbers for get average
+ */
 export const avg = (nums = []) => {
   if (!arrayOfNumbers(nums)) return;
 
