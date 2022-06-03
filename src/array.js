c57c8ba6e34d81500e39519ca34187506557a21f
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-03 12:50:50 -0500

    Fix: Fixed not correctly random sort of sort() in array.js. Feat: Added jsdoc comments

--- src/array.js
+++ src/array.js
@@ -1,3 +1,7 @@
+/**
+ * Check if an array is an array of numbers, returning true or false as appropiate
+ * @param  {Array<Number>}  arr Array to check
+ */
 export const arrayOfNumbers = (arr = []) => {
   if (!Array.isArray(arr)) return false;
 
@@ -10,6 +14,10 @@ export const arrayOfNumbers = (arr = []) => {
   return true;
 };
 
+/**
+ * Check if an array is an array of strings, returning true or false as appropiate
+ * @param  {Array<String>}  arr Array to check
+ */
 export const arrayOfStrings = (arr = []) => {
   if (!Array.isArray(arr)) return false;
 
@@ -22,6 +30,10 @@ export const arrayOfStrings = (arr = []) => {
   return true;
 };
 
+/**
+ * Returns a random element from an array
+ * @param  {Array} arr Array from which an element will be retrieved
+ */
 export const randomElement = (arr) => {
   if (!Array.isArray(arr)) return;
 
@@ -32,6 +44,12 @@ export const randomElement = (arr) => {
 
 const validOrders = [1, -1, 'r'];
 
+/**
+ * Returns a new array sorted according to the specified order
+ * @param  {Array<String|Number>}  arr   Array to sort
+ * @param  {String|Number} order Specific order
+ * @return {Array<String|Number>}       Sorted array
+ */
 export const sort = (arr = [], order = 1) => {
   if (!arrayOfNumbers(arr) && !arrayOfStrings(arr)) return;
 
@@ -42,7 +60,8 @@ export const sort = (arr = [], order = 1) => {
   if (arrayOfNumbers(arr)) {
     if (order === 1) return arr.map((el) => el).sort((a, b) => a - b);
     if (order === -1) return arr.map((el) => el).sort((a, b) => b - a);
-    if (order === 'r') return arr.map((el) => el).sort(() => Math.random());
+    if (order === 'r')
+      return arr.map((el) => el).sort(() => 0.5 - Math.random());
   }
 
   if (order === 1) return arr.map((el) => el).sort();
@@ -54,5 +73,5 @@ export const sort = (arr = [], order = 1) => {
         if (b > a) return 1;
         return 0;
       });
-  if (order === 'r') return arr.map((el) => el).sort();
+  if (order === 'r') return arr.map((el) => el).sort(() => 0.5 - Math.random());
 };
