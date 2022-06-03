c632b9c0bded84f02a3df22a11890bdc12f0dd9a
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-03 18:32:28 -0500

    Added jsdoc comments to formInputs() of form.js

--- src/form.js
+++ src/form.js
@@ -1,1 +1,6 @@
+/**
+ * Retrieves data as an object from a HTMLFormElement
+ * @param  {HTMLFormElement} form Form element
+ * @return {{}}      Data returned as an object
+ */
 export const formInputs = (form) => Object.fromEntries(new FormData(form));
