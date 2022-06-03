11fcc6f31c526e2b535e2d89f2ef3a3cab377492
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-03 17:58:21 -0500

    Added jsdoc comments to gid(), qs() and qsa() of dom.js

--- src/dom.js
+++ src/dom.js
@@ -1,9 +1,23 @@
 const d = document;
 
-export const gid = (id, parent = d) => parent.getElementById(id);
+/**
+ * Returns a reference to the first object with the specified value of the ID attribute.
+ * @param  {String} id String that specifies the ID value.
+ */
+export const gid = (id) => d.getElementById(id);
 
+/**
+ * Returns the first element that is a descendant of node that matches selectors.
+ * @param  {String} selector A valid css selector
+ * @param  {Document|Element|DocumentFragment} parent   Parent Node
+ */
 export const qs = (selector, parent = d) => parent.querySelector(selector);
 
+/**
+ * Returns all element descendants of node that match selectors.
+ * @param  {String} selector A valid css selector
+ * @param  {Document|Element|DocumentFragment} parent   Parent Node
+ */
 export const qsa = (selector, parent = d) => [
   ...parent.querySelectorAll(selector),
 ];
