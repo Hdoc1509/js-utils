bca5466ffdf31de1c4e072cfbc531b9b32533c03
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-04 12:58:39 -0500

    Fix: Changed relative paths of exports in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-export { default as Arith, sum, avg } from './src/arith.js';
+export { default as Arith, sum, avg } from './arith.js';
 export {
   arrayOfNumbers,
   arrayOfStrings,
   randomElement,
   sort,
-} from './src/array.js';
-export { default as DOM, gid, qs, qsa } from './src/dom.js';
-export { default as EQN } from './src/equation.js';
-export { formInputs } from './src/form.js';
+} from './array.js';
+export { default as DOM, gid, qs, qsa } from './dom.js';
+export { default as EQN } from './equation.js';
+export { formInputs } from './form.js';
