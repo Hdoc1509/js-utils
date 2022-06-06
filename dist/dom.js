c1a9860c8ab23e1607f635e72c3b62a696ac3194
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-06 17:54:32 -0500

    Removed dist folder

--- dist/dom.js
+++
@@ -1,1 +0,0 @@
-var o=document,r=e=>o.getElementById(e),c=(e,t=o)=>t.querySelector(e),l=(e,t=o)=>[...t.querySelectorAll(e)],n={gid:r,qs:c,qsa:l};export{n as default,r as gid,c as qs,l as qsa};
--- dist/form.js
+++
@@ -1,1 +0,0 @@
-var o=t=>Object.fromEntries(new FormData(t));export{o as formInputs};
