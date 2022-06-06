cee396611fba33e2a700ec47563f3e719162b043
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-06 18:33:34 -0500

    Changed syntax to CommonJS in index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-export { default as Arith, sum, avg } from './arith.js';
-export {
-  arrayOfNumbers,
-  arrayOfStrings,
-  randomElement,
-  sort,
-} from './array.js';
-export { default as DOM, gid, qs, qsa } from './dom.js';
-export { default as EQN } from './equation.js';
-export { formInputs } from './form.js';
+exports.arith = require('./arith');
+exports.array = require('./array');
+exports.dom = require('./dom');
+exports.equation = require('./equation');
+exports.form = require('./form');
