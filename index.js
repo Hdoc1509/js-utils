4e64f374ad878f8232263d6bf306424a2e4ea6c0
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-06 18:35:18 -0500

    Fix: Changed relative path for utils

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-exports.arith = require('./arith');
-exports.array = require('./array');
-exports.dom = require('./dom');
-exports.equation = require('./equation');
-exports.form = require('./form');
+exports.arith = require('./utils/arith');
+exports.array = require('./utils/array');
+exports.dom = require('./utils/dom');
+exports.equation = require('./utils/equation');
+exports.form = require('./utils/form');
