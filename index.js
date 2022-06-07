5b699cfb31300b740257b8f3dce79a2b3bd6ea82
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-06 19:08:06 -0500

    Changed method to export utils

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-exports.arith = require('./utils/arith');
-exports.array = require('./utils/array');
-exports.dom = require('./utils/dom');
+const { sum, avg } = require('./utils/arith');
+const {
+  arrayOfNumbers,
+  arrayOfStrings,
+  sort,
+  randomElement,
+} = require('./utils/array');
+const { gid, qs, qsa } = require('./utils/dom');
+const { formInputs } = require('./utils/form');
+
 exports.equation = require('./utils/equation');
-exports.form = require('./utils/form');
+module.exports = {
+  sum,
+  avg,
+  arrayOfNumbers,
+  arrayOfStrings,
+  sort,
+  randomElement,
+  gid,
+  qs,
+  qsa,
+  formInputs,
+};
